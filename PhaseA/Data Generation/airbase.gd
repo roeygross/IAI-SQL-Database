49d07308ec25c75ee7f334e0cc3e31f72d4854e3
Ykoler <126462535+Ykoler@users.noreply.github.com>
2024-05-21 01:47:15 +0300

    Create airbase.gd

---
+++ PhaseA/Data Generation/airbase.gd
@@ -0,0 +1,53 @@
+
+[General]
+Version=1
+
+[Preferences]
+Username=
+Password=2105
+Database=
+DateFormat=
+CommitCount=0
+CommitDelay=0
+InitScript=
+
+[Table]
+Owner=SYSTEM
+Name=AIRBASE
+Count=400..450
+
+[Record]
+Name=NAME
+Type=VARCHAR2
+Size=20
+Data=FirstName +' '+ LastName +' '+ 'AirBase'
+Master=
+
+[Record]
+Name=CAPACITY
+Type=NUMBER
+Size=
+Data=Random(10, 100)
+Master=
+
+[Record]
+Name=LOCATION
+Type=VARCHAR2
+Size=30
+Data=City
+Master=
+
+[Record]
+Name=SQUADRON_NUMBER
+Type=NUMBER
+Size=
+Data=List(select squadron_number from squadron)
+Master=
+
+[Record]
+Name=RUNWAY_NUMBER
+Type=NUMBER
+Size=
+Data=Random(1, 20)
+Master=
+
