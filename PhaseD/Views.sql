644a7dd9b83214ab993cb0ba838a1d322fe88c7b
Ykoler <126462535+Ykoler@users.noreply.github.com>
2024-07-15 23:25:15 +0300

    Update Views.sql

--- PhaseD/Views.sql
+++ PhaseD/Views.sql
@@ -11,6 +11,13 @@ SELECT shift_id, start_date_time, end_date_time, base_location, COUNT(volunteer_
 FROM Volunteers_Shifts
 GROUP BY shift_id, start_date_time, end_date_time, base_location;
 
+-- get an image of shifts for every volunteer in the comming year
+SELECT volunteer_id, volunteer_name, shift_id, start_date_time, end_date_time, base_location
+FROM Volunteers_Shifts
+WHERE start_date_time > to_date('01/01/2024', 'DD/MM/YYYY')
+ORDER BY start_date_time ASC;
+
+
 
 -- the engineering crew wants an image of which aircrafts need maintenece
 CREATE OR REPLACE VIEW Maintenance_Aircraft_Check AS
@@ -25,4 +32,9 @@ SELECT serialid, production_date, ammunitiontype, model
 FROM Maintenance_Aircraft_Check
 WHERE production_date <= ADD_MONTHS(SYSDATE, -480);
 
+-- list the most commonly used ammunition type
+SELECT ammunitiontype, COUNT(*) AS usage_count
+FROM Maintenance_Aircraft_Check
+GROUP BY ammunitiontype
+ORDER BY usage_count DESC;
 
